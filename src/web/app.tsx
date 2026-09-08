f68dfb2bda01c5ee0cf73b99628c4ef12e2a1147
Fabian Wiles <fabianwiles@canva.com>
2026-09-09 07:38:53 +1000

    Compose browser domain actions at app root

--- src/web/app.tsx
+++ src/web/app.tsx
@@ -2,8 +2,10 @@ import { useEffect, useMemo, useRef } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { AppConnection } from "./app-connection.js";
+import { DmmActions } from "./dmm/dmm-actions.js";
 import { DmmBinding } from "./dmm/dmm-binding.js";
 import { DmmRoute } from "./dmm/dmm-route.js";
+import { ScopeActions } from "./scope-actions.js";
 import { ScopeBinding } from "./scope-binding.js";
 import { ScopeRoute } from "./scope-route.js";
 import { WaveformController } from "./waveform/waveform-controller.js";
@@ -28,21 +30,36 @@ export function App() {
     return created;
   }, [connection, controller]);
   const dmmBinding = useMemo(() => new DmmBinding(connection), [connection]);
+  const scopeActions = useMemo(() => new ScopeActions(scopeBinding), [scopeBinding]);
+  const dmmActions = useMemo(() => new DmmActions(dmmBinding), [dmmBinding]);
 
   useEffect(() => {
     connection.connect();
     return () => {
+      scopeActions.dispose();
       scopeBinding.dispose();
       dmmBinding.dispose();
       connection.dispose();
     };
-  }, [connection, dmmBinding, scopeBinding]);
+  }, [connection, dmmBinding, scopeActions, scopeBinding]);
 
   return (
     <main className="app-shell">
       <Routes>
-        <Route path="/" element={<ScopeRoute binding={scopeBinding} controller={controller} />} />
-        <Route path="/dm858e" element={<DmmRoute binding={dmmBinding} />} />
+        <Route
+          path="/"
+          element={(
+            <ScopeRoute
+              binding={scopeBinding}
+              actions={scopeActions}
+              controller={controller}
+            />
+          )}
+        />
+        <Route
+          path="/dm858e"
+          element={<DmmRoute binding={dmmBinding} actions={dmmActions} />}
+        />
       </Routes>
     </main>
   );
